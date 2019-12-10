756bc0c5a631974160a0cb32473096c66c8cfe1e
claudio-cesar-fraga-cavalcante <claudio.cavalcante@gmail.com>
2019-12-10 11:43:23 +0000

    Conceitos parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaiComponent } from './pai/pai.component';
+import { FilhoComponent } from './filho/filho.component';
+import { Filho2Component } from './filho2/filho2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaiComponent,
+    FilhoComponent,
+    Filho2Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/filho2/filho2.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-filho2',
+  templateUrl:'filho2.component.html',
+  styleUrls: ['filho2.component.css']
+})
+export class Filho2Component implements OnInit {
+
+  ngOnInit() {
+
+  }
+}
